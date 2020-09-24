1528275eada96d4206b5701ef21eba2828f6002c
Jarom Lybbert <lyb18003@byui.edu>
2020-09-23 22:38:17 -0600

    home page draft added

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
 <?php
 
-  phpinfo();
+  //Home Controller
+
+if (isset($_GET['action'])) {
+  $action = $_GET['action'];
+}
+else if (isset($_POST['action'])) {
+  $action = $_POST['action'];
+}
+
+switch ($action) {
+    default:
+    include "views/ocHome.php";
+break;
+}
 
 ?>
---
+++ web/views/ocHome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/myOCsStyle.css">
+    <title>My OCs</title>
+</head>
+<body>
+    <header>
+        <h1>My OCs</h1>
+        <p>Date: <?php echo date('d/m/y'); ?></p>
+        <a>Assignments</a>
+    </header>
+    <main>
+        <p id="description">These are my OCs!</p>
+        <div id="OCpics">
+            <img id="imgPhoenix" src="../images/Phoenix with Ladybugs by AmyGamy.jpg" alt="Phoenix Gleam">
+            <img id="imgSilver" src="../images/Silverbelle Smug.png" alt="Silverbelle">
+            <img id="imgCotton" src="../images/Cotton Stream.PNG">
+            <img id="imgNeon" src="../images/Neon by Neondromeda.jpg" alt="Neon">
+        </div>
+    </main>
+</body>
+</html>
