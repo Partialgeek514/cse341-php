f1558bc6a12ea33c9cb29d806cef63e08664e2ba
Jarom Lybbert <lyb18003@byui.edu>
2020-09-25 20:05:25 -0600

    Modified captions for images

--- web/captions.php
+++ web/captions.php
@@ -11,16 +11,16 @@ else if (isset($_POST['character'])) {
 
 switch ($character) {
     case "imgPhoenix":
-        echo "Phoenix Gleam (Unicorn/Male)";
+        echo "Phoenix Gleam (Unicorn/Male)<br>Art by <a href='https://www.deviantart.com/amy-gamy'>Amy-Gamy</a>";
     break;
     case "imgSilver":
-        echo "Silverbelle (Pegasus/Female)";
+        echo "Silverbelle (Pegasus/Female)<br>Art by <a href='https://www.deviantart.com/kimjoman'>Kimjoman</a>";
     break;
     case "imgCotton":
-        echo "Cotton Stream (Griffin/Female)";
+        echo "Cotton Stream (Griffin/Female)<br>Art by <a href='https://twitter.com/_HarlowPuppy_'>Harlow</a>";
     break;
     case "imgNeon":
-        echo "Neon (Pegasus/Female)";
+        echo "Neon (Pegasus/Female)<br>Art by <a href='https://twitter.com/MLPNeondromeda'>Neondromeda</a>";
     break;
     default:
     echo "(No Caption)";
--- web/js/myOCsScript.js
+++ web/js/myOCsScript.js
@@ -4,10 +4,14 @@ $(".clickModel").click(function() {
     var imgSrc = activeImg.attr("src");
     $("#modelImg").attr("src", imgSrc);
     $("#model").css("display", "block");
+    $("#caption").html("<p>Loading...</p>");
     $.get("../captions.php", {character:imgId}, function(data, status, xhr) {
         console.log(status);
         if (status == "success") {
             $("#caption").html("<p>" + data + "</p>");
+        }
+        else {
+            $("#caption").html("<p>Error loading caption :(</p>");
         }
     })
 })
