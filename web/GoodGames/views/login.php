d0cd9cb0be1441f04dc6671d2b83ce210997f138
Jarom Lybbert <lyb18003@byui.edu>
2020-10-28 17:21:29 -0600

    test

--- web/GoodGames/views/login.php
+++ web/GoodGames/views/login.php
@@ -26,7 +26,7 @@
             <h2>Login</h2>
         </div>
         <div class="feature">
-            <form action="/GoodGames/accounts" method="GET">
+            <form action="/GoodGames/accounts" method="POST">
                 <label for="username">Username</label><br>
                 <input type="text" name="username"><br>
                 <label for="password">Password</label><br>
