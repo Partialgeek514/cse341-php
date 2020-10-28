39ca1c9d250fdeacc6250f03a5ec0665ee009069
Jarom Lybbert <lyb18003@byui.edu>
2020-10-28 16:49:08 -0600

    test

--- web/GoodGames/index.php
+++ web/GoodGames/index.php
@@ -11,6 +11,13 @@ if ($_ENV['MEMCACHIER_USERNAME']) {
 session_start();
 //phpinfo();
 //exit;
+if (!isset($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+}
+$_SESSION['count']++;
+
+echo "Hello #" . $_SESSION['count'];
+exit;
 include_once $_SERVER['DOCUMENT_ROOT'] . '/GoodGames/functions.php';
 
 if (isset($_GET['action'])) {
