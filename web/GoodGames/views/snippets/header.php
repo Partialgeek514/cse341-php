938666f857f6bd0cbd83bb024ea56bb3125b1c90
Jarom Lybbert <lyb18003@byui.edu>
2020-10-28 16:58:38 -0600

    test

--- web/GoodGames/views/snippets/header.php
+++ web/GoodGames/views/snippets/header.php
@@ -1,6 +1,6 @@
 <h1 class="logo">GoodGames</h1>
 <?php if (isset($funnyQuote)) {echo "<p id='quote'>$funnyQuote</p>";} ?>
-<?php if (isset($_SESSION['loggedIn']) and $_SESSION['loggedIn']) {
+<?php if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) {
         echo "<a href='/GoodGames/accounts?action=logout'>Logout</a>";
     }
     else {
