5b3eb39f20b00f052b4e5f78af8d890de7e655f1
Jarom Lybbert <lyb18003@byui.edu>
2020-10-28 14:25:49 -0600

    Added redis capibility

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3ddccd00026fb7dfe44a87b0e8527ad21326f769',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ddccd00026fb7dfe44a87b0e8527ad21326f769',
+    ),
+  ),
+);
